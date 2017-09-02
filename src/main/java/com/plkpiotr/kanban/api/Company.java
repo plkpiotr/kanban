ef81ca809987219b69733bce4b5c43e05a4fbb9f
plkpiotr <piotrp10@o2.pl>
2017-09-02 23:08:49 +0200

    Add getters and setters in 'Company' class

--- src/main/java/com/plkpiotr/kanban/api/Company.java
+++ src/main/java/com/plkpiotr/kanban/api/Company.java
@@ -23,4 +23,36 @@ public class Company {
 
     @OneToMany(mappedBy = "company")
     private List<Employee> listOfEmployees;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Project> getListOfProjects() {
+        return listOfProjects;
+    }
+
+    public void setListOfProjects(List<Project> listOfProjects) {
+        this.listOfProjects = listOfProjects;
+    }
+
+    public List<Employee> getListOfEmployees() {
+        return listOfEmployees;
+    }
+
+    public void setListOfEmployees(List<Employee> listOfEmployees) {
+        this.listOfEmployees = listOfEmployees;
+    }
 }
